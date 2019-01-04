bef13cb0b377efa4b31174f0715a2b4cdeb97b25
Ashley Straw <as.fireflash38@gmail.com>
2019-01-04 15:06:06 -0500

    Fixing entrypoints & packagin

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
+import glob
+from os.path import splitext, basename
 from setuptools import setup
 
 setup(
     name="pytest-docgen",
-    packages=['pytest_docgen', 'sphinxext'],
+    packages=['pytest_docgen'],
+    py_modules=[splitext(basename(i))[0] for i in glob.glob('pytest_docgen/sphinxext')],
     version="1.1.0",
     # the following makes a plugin available to pytest
     entry_points={
         'pytest11': [
             'pytest_docgen = pytest_docgen.pytest_docgen',
         ],
-        'sphinx.builders': [
-            'pytest_docgen = pytest_docgen.sphinxext.collapsible_block',
-        ]
     },
 
     # custom PyPI classifier for pytest plugins
